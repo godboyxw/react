3d6f6aab3fd0cc46a2a10f5374daff682d9f60a6
xiangwei <815950959@.com>
2019-05-08 18:25:20 +0800

    add

--- restudy/src/Components/js/Demo.js
+++ restudy/src/Components/js/Demo.js
@@ -1,18 +1,24 @@
+// 引入react 父类实例
 import React,{Component} from 'react'
 import '../css/demo.css'
 
 class Demo extends Component{
+    //构造函数 继承
     constructor(props){
         super(props)
         this.state={
             date : new Date()
         }
     }
+
+    // DOM节点渲染
     render(){
         return(
             <h1>现在的时间是{this.state.date.toLocaleTimeString()}</h1>
         )
     }
+
+    // 生命周期
     componentDidMount(){
         this.timer = setInterval(()=>{
              this.tick()
@@ -21,6 +27,8 @@ class Demo extends Component{
      componentWillUnmount(){
          clearInterval(this.timer)
      }
+
+     // 方法
      tick(){
          this.setState({
              date : new Date()
--- restudy/src/index.js
+++ restudy/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/js/App';
 import Demo from './Components/js/Demo';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <div>
@@ -15,4 +15,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
